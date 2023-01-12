9be1120a9a7708611b4825d1d4894d5f61cdb1d4
DEVEV <everly.jang@gmail.com>
2023-01-12 15:53:57 +0900

    Communicating Events: emit add to cart and remove from cart event from product display component

--- Intro-to-Vue-3-L2-start/components/ProductDisplay.js
+++ Intro-to-Vue-3-L2-start/components/ProductDisplay.js
@@ -55,10 +55,10 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.cart++;
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
         },
         removeFromCart() {
-            this.cart--;
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
         },
         updateVariant(index) {
             this.selectedVariant = index;
