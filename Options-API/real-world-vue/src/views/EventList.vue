4f9015175cf414f75e7c2e82e970ce306b86f6f7
DEVEV <everly.jang@gmail.com>
2023-01-13 17:41:14 +0900

    Modularize API code into a service layer
    so that each component doesn't have to create their own Axios instance

--- Options-API/real-world-vue/src/views/EventList.vue
+++ Options-API/real-world-vue/src/views/EventList.vue
@@ -9,7 +9,8 @@
 /* eslint-disable */
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService.js'
+
 export default {
   name: 'EventList',
   components: {
@@ -21,9 +22,7 @@ export default {
     }
   },
   created() {
-    axios.get(
-      'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events'
-      )
+    EventService.getEvents()
       .then(response => {
         this.events = response.data
       })
