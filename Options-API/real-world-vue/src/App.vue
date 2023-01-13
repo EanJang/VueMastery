4f7cb68db172f19d1986f4968a56f2d1c015c1ed
DEVEV <everly.jang@gmail.com>
2023-01-13 15:58:13 +0900

    Add title for the Home view

--- Options-API/real-world-vue/src/App.vue
+++ Options-API/real-world-vue/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <h1>Events For Good</h1>
+    <router-view />
+  </div>
 </template>
 
 <style>
