523a22c0b34e1a492ab33ef5b871238835071a96
DEVEV <everly.jang@gmail.com>
2023-01-16 10:13:54 +0900

    Dynamic Routing: Add Event Detail component and link to Event List page

--- Options-API/real-world-vue/src/services/EventService.js
+++ Options-API/real-world-vue/src/services/EventService.js
@@ -10,10 +10,13 @@ const apiClient = axios.create({
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
-  });
+});
 
 export default {
     getEvents() {
         return apiClient.get('/events')
+    },
+    getEvent(id) {
+        return apiClient.get('/events/' + id)
     }
 }
