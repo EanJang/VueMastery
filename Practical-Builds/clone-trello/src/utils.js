80889e47a87da274027b061ebf4434fa158ab34e
DEVEV <everly.jang@gmail.com>
2023-01-16 17:04:05 +0900

    Update saveStatePlugin in utils.js

--- Practical-Builds/clone-trello/src/utils.js
+++ Practical-Builds/clone-trello/src/utils.js
@@ -1,9 +1,15 @@
-export function uuid () {
+export function uuid() {
   return Math.random().toString(16).slice(2)
 }
 
-export function saveStatePlugin (store) {
+// listen for mutations being made to our Vuex store, and store them in the local Storage.
+export function saveStatePlugin(store) {
   store.subscribe(
-    (mutation, state) => localStorage.setItem('boards', JSON.stringify(state.boards))
+    (mutation, state) => {
+      localStorage.setItem(
+        'board',
+        JSON.stringify(state.board)
+      )
+    }
   )
 }
