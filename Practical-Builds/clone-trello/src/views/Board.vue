5e92c15fa5317e6a2aeacd5ebaf3afc4ab219248
DEVEV <everly.jang@gmail.com>
2023-01-19 09:36:04 +0900

    Add a method to close task modal

--- Practical-Builds/clone-trello/src/views/Board.vue
+++ Practical-Builds/clone-trello/src/views/Board.vue
@@ -33,6 +33,7 @@
     <div
       class="task-bg"
       v-if="isTaskOpen"
+      @click.self = "close"
     >
       <router-view />
     </div>
@@ -52,6 +53,9 @@ export default {
   methods: {
     goToTask (task) {
       this.$router.push({ name: 'task', params: { id: task.id } })
+    },
+    close () {
+      this.$router.push({ name: 'board' })
     }
   }
 }
